519bb44382931d6d137308d4b170a3188cdb33cc
Laura Ximena Velasco Rocha <116115416+XimenaVelascoro@users.noreply.github.com>
2023-01-17 14:53:19 -0500

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,14 @@
+//aqui va la información dde cada producto
+//que va a representar un diccionario u obj
+//EL ID NO PUEDE REPETIRSE,
+//puede ir incrementando pero no se puyede repetir 
+export const dataEntradas = [
+  {
+    id: 1,
+    nameProduct: "Antipasto",
+    description: "Mozzarella di bufala, tomate seco, berenjena, pimenton, champiñon y tahini de garbanzo",
+    price: 25000,
+    img: " ",
+    quantity:1,
+  }
+];
