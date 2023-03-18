ca091229cea14ea6dfc0fb4920aca8fc721659f9
Laura Ximena Velasco Rocha <12ximenavelasco@gmail.com>
2023-03-18 01:59:55 -0500

    its meee again_

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import "./Header.css";
 
+
 export default function Header() {
   return (
     <div className="container_header">
       <h1 href="#/logo" className="logop"> </h1>
       <h1 className="options">
-        <a href="#/menu">MENU</a>
-        <a href="#/carrito"> CARRITO </a>
-        <a href="#/contacto">CONTACTO</a>
+        <div className="link-container"><a  className='menu' href="#/menu"></a>
+        <small> Hablemos</small></div>
+        <div className="link-container"><a className='car' href="#/carrito">  </a>
+        <small> Hablemos</small>
+        </div>
+       <div className="link-container"> <a className='cell' href="#/contacto"></a>
+       <small> Hablemos</small>
+       </div>
       </h1>
     </div>
   );
